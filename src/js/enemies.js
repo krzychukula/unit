0feb5e95070915dfe0b9c480b58b4d532d12f659
Krzysztof Kula <krzychukula@gmail.com>
2014-05-19 19:15:42 +0200

    removing of killed enemies and callback on update

--- src/js/enemies.js
+++ src/js/enemies.js
@@ -14,6 +14,15 @@
 			//this.gravity(this.collection[i]);
 			this.collection[i].update();
 			//this.collideWithWorld(this.collection[i]);
+			callback(this.collection[i]);
+		};
+	};
+
+	Enemies.prototype.removeKilled = function() {
+		for (var i = this.collection.length - 1; i >= 0; i--) {
+			if(this.collection[i].killed){
+				this.collection.splice(i, 1);
+			}
 		};
 	};
 
